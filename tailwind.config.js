87a1d0193ff73e4907d4670e0b78645d697d5e5d
Razvan Andrei <chiper_raizy@yahoo.com>
2025-02-17 15:22:50 +0100

    adding colorPalette and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F29F58", // main color
+        secondary: "#1B1833", // secondary color
+        tertiary: "#AB4459", //tertiary color
+        accent: "#441752", // accent color
+      },
+      fontFamily: {
+        serif: ["Ubuntu", "sans-serif"], // set Ubuntu as the default font
+      },
+    },
   },
   plugins: [daisyui],
 };
